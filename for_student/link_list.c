d6d3c807d6863f8e60754a4b4d6e4f77eeb9277a
pppppppphoebe <open1614@gmail.com>
2022-04-26 12:06:08 +0800

    Update link_list.c

--- for_student/link_list.c
+++ for_student/link_list.c
@@ -25,6 +25,7 @@ void my_insert(struct node* HEAD, int ID, char *Name, int node_id){
 		printf("INVALID OPERATION\n");
 	}
 }
+
 void my_delete(struct node* HEAD, char *Name){
 	// Delete the node which store the Name.
 	// If it doesn’t exit such Name, print “INVALID OPERATION”.
@@ -46,6 +47,7 @@ void my_delete(struct node* HEAD, char *Name){
 		free(cur);
 	}
 }
+
 void my_swap(struct node *HEAD, char *Name1, char *Name2){
 	// Swap the node which store Name1 and Name2
 	// If either of the node doesn’t exit, print “INVALID OPERATION”
@@ -91,17 +93,8 @@ void my_swap(struct node *HEAD, char *Name1, char *Name2){
 		p2->next = p1->next;
 		p1->next = temp;		
 	}
-	/*
-	for(ptr = HEAD ; ptr != NULL && strcmp(ptr->name,Name1) != 0; ptr = ptr->next){
-		if(strcmp(ptr->name,Name1) == 0){
-			p1 = ptr;
-		}
-		if(strcmp(ptr->name,Name2) == 0){
-			p2 = ptr;
-		}
-	}*/
-
 }
+
 void my_print_id(struct node *HEAD, char *Name){
 	// Print the id of the corresponding Name.
 	// If Name doesn’t exit, print “INVALID OPERATION”
@@ -113,6 +106,7 @@ void my_print_id(struct node *HEAD, char *Name){
 		printf("INVALID OPERATION\n");
 	}
 }
+
 void my_print_name(struct node *HEAD, int ID){
 	// Print the name of the corresponding ID
 	// If ID doesn’t exit, print “INVALID OPERATION”
@@ -124,6 +118,7 @@ void my_print_name(struct node *HEAD, int ID){
 		printf("INVALID OPERATION\n");
 	}
 }
+
 void my_result(struct node* HEAD){
 	// Print out all the information in the linked list orderly
 	// in the format “ID NAME”. (start from HEAD)
